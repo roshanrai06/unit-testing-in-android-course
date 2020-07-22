862e4955d756dae24d9b79cbb3d8ee6a45fc18fe
Roshan Rai <rrai@mast.com>
2020-07-22 19:17:37 +0530

    String Duplicator Unit Test

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,36 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
-public class StringDuplicatorTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class StringDuplicatorTest {
+    StringDuplicator SUT;
+
+    @Before
+    public void setup() {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_emptyString_emptyStringReturned() {
+        String result = SUT.duplicate("");
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void duplicate_singleString_singleStringReturned() {
+        String result = SUT.duplicate("a");
+        assertThat(result, is("aa"));
+
+    }
+
+    @Test
+    public void duplicate_String_StringReturned() {
+        String result = SUT.duplicate("Roshan Rai");
+        assertThat(result, is("Roshan RaiRoshan Rai"));
+
+    }
 }
